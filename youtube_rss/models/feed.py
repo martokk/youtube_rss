0d97d86104c7289f3566b409136618d9e29b7049
martokk <25113632+martokk@users.noreply.github.com>
2022-12-29 01:43:12 -0600

    removing Feed model

--- youtube_rss/models/feed.py
+++
@@ -1,7 +0,0 @@
-from sqlmodel import Field, SQLModel
-
-
-class Source(SQLModel, table=True):
-    source_id: str = Field(primary_key=True, nullable=False)
-    name: str = Field()
-    url: str = Field()
