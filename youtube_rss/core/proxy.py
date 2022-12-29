d79f9661add8fe966be0c75b97e26ef3c59b2fa2
martokk <25113632+martokk@users.noreply.github.com>
2022-12-29 10:29:00 -0600

    moving to async context manager

--- youtube_rss/core/proxy.py
+++ youtube_rss/core/proxy.py
@@ -2,15 +2,12 @@ import httpx
 from starlette.background import BackgroundTask
 from starlette.responses import StreamingResponse
 
-client = httpx.AsyncClient()
-
 
 async def reverse_proxy(url: str) -> StreamingResponse:
-    url = httpx.URL(url=url)
-    print(url)
+    async with httpx.AsyncClient() as client:
+        rp_req = client.build_request("GET", url=url)
+        rp_resp = await client.send(rp_req, stream=True)
 
-    rp_req = client.build_request("GET", url=url)
-    rp_resp = await client.send(rp_req, stream=True)
     return StreamingResponse(
         rp_resp.aiter_raw(),
         status_code=rp_resp.status_code,
