562607175bd507314884e00bd24e73404e4b28ff
martokk <25113632+martokk@users.noreply.github.com>
2023-01-06 23:41:35 -0600

    fixing media, proxy

--- youtube_rss/api/v1/endpoints/media.py
+++ youtube_rss/api/v1/endpoints/media.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, HTTPException, Request, status
-from fastapi.responses import StreamingResponse
+from fastapi.responses import RedirectResponse, Response
 
 from youtube_rss.core.proxy import reverse_proxy
 from youtube_rss.crud.video import video_crud
+from youtube_rss.handlers import get_handler_from_string
 
 router = APIRouter()
 
 
 @router.get("/{video_id}")
 @router.head("/{video_id}")
-async def reverse_proxy_video_id(video_id: str, request: Request) -> StreamingResponse:
+async def reverse_proxy_video_id(video_id: str, request: Request) -> Response:
     """
     Streams the response of yt-dlp
     """
@@ -19,4 +20,7 @@ async def reverse_proxy_video_id(video_id: str, request: Request) -> StreamingRe
             status_code=status.HTTP_202_ACCEPTED,
             detail="The server has not yet retrieved a media_url from yt-dlp.",
         )
-    return await reverse_proxy(url=video.media_url, request=request)
+    handler = get_handler_from_string(handler_string=video.handler)
+    if handler.USE_PROXY:
+        return await reverse_proxy(url=video.media_url, request=request)
+    return RedirectResponse(url=video.media_url)
--- youtube_rss/handlers/__init__.py
+++ youtube_rss/handlers/__init__.py
@@ -3,11 +3,6 @@ from urllib.parse import ParseResult, urlparse
 from .base import ServiceHandler
 from .youtube import YoutubeHandler
 
-# def get_handler_from_extractor(extractor: str) -> ServiceHandler:
-#     if extractor == "YoutubeTab":
-#         return YoutubeHandler()
-#     return ServiceHandler()
-
 
 def get_handler_from_url(url: str | ParseResult) -> ServiceHandler:
     url = url if isinstance(url, ParseResult) else urlparse(url=url)
@@ -17,6 +12,7 @@ def get_handler_from_url(url: str | ParseResult) -> ServiceHandler:
     return ServiceHandler()
 
 
-def get_handler_from_handler_string(handler_string: str) -> ServiceHandler:
-    handler_cls = globals()[handler_string]
-    return handler_cls()
+def get_handler_from_string(handler_string: str) -> ServiceHandler:
+    if handler_string == "YoutubeHandler":
+        return YoutubeHandler()
+    return ServiceHandler()
