ae274b3ba2f6f8494461757bd429a9ac6975d871
martokk <25113632+martokk@users.noreply.github.com>
2023-01-06 20:49:02 -0600

    adding add_new_source_videos_from_fetched_videos

--- youtube_rss/crud/source.py
+++ youtube_rss/crud/source.py
@@ -1,15 +1,18 @@
+from loguru import logger
 from sqlmodel import Session
 
 from youtube_rss.core.debug_helpers import timeit
 from youtube_rss.crud.exceptions import RecordAlreadyExistsError
-from youtube_rss.crud.video import refresh_videos, video_crud
+from youtube_rss.crud.video import VideoCRUD, refresh_videos
 from youtube_rss.db.database import engine
 from youtube_rss.models.source import Source, SourceCreate, SourceRead
+from youtube_rss.models.video import Video
 from youtube_rss.services.feed import build_rss_file
 from youtube_rss.services.source import (
     generate_source_id_from_url,
     get_source_from_source_info_dict,
     get_source_info_dict,
+    get_source_videos_from_source_info_dict,
 )
 
 from .base import BaseCRUD
@@ -75,37 +78,34 @@ class SourceCRUD(BaseCRUD[Source, SourceCreate, SourceRead]):
             extract_flat=True,
             use_cache=False,
         )
-        fetched_source = await get_source_from_source_info_dict(source_info_dict=source_info_dict)
 
-        # Save the source to the database
-        db_source = await self.update(SourceCreate(**fetched_source.dict()), id=fetched_source.id)
+        # Update Source Videos from Fetched Videos
+        db_source = await self.get(id=source_id)
+        # fetched_videos = fetched_source.videos
+        fetched_videos = await get_source_videos_from_source_info_dict(
+            source_info_dict=source_info_dict
+        )
 
-        # Delete orphaned Videos no longer in the Source
-        await self.delete_orphaned_source_videos(fetched_source=fetched_source, db_source=db_source)
-
-        # Fetch video information from yt-dlp for new videos
-        await refresh_videos(videos=db_source.videos)
+        added_videos = await add_new_source_videos_from_fetched_videos(
+            fetched_videos=fetched_videos, db_source=db_source
+        )
+        deleted_videos = await delete_orphaned_source_videos(
+            fetched_videos=fetched_videos, db_source=db_source
+        )
+        refreshed_videos = await refresh_videos(videos=db_source.videos)
 
         # Build RSS File
         await build_rss_file(source=db_source)
 
-        return await self.get(id=fetched_source.id)
+        logger.success(
+            f"Completed fetching Source(id='{db_source.id}'). "
+            f"[{len(added_videos)}/{len(deleted_videos)}/{len(refreshed_videos)}] "
+            f"Added {len(added_videos)} new videos. "
+            f"Deleted {len(deleted_videos)} orphaned videos. "
+            f"Refreshed {len(refreshed_videos)} videos."
+        )
 
-    async def delete_orphaned_source_videos(
-        self, fetched_source: Source, db_source: Source
-    ) -> None:
-        """Delete videos that are no longer present in `fetched_source`.
-
-        Args:
-        - fetched_source: The source object to compare the videos against.
-        - db_source: The source object in the database to delete videos from.
-        """
-        fetched_video_ids = [video.id for video in fetched_source.videos]
-
-        # Iterate through the videos in the source in the database
-        for db_video in db_source.videos:
-            if db_video.id not in fetched_video_ids:
-                await video_crud.delete(id=db_video.id)
+        return await self.get(id=source_id)
 
     async def fetch_source_videos(self, source_id: str) -> Source:
         """Fetch new data from yt-dlp for each video in the source.
@@ -129,29 +129,79 @@ class SourceCRUD(BaseCRUD[Source, SourceCreate, SourceRead]):
 
 with Session(engine) as _session:
     source_crud = SourceCRUD(session=_session)
+    video_crud = VideoCRUD(session=_session)
 
 
-@timeit
 async def refresh_all_sources() -> list[Source]:
     """
-    Fetches new data for all sources from yt-dlp.
+    Fetches new data from yt-dlp for all Sources.
 
     Returns:
-        The refreshed list of sources.
+        The list of refreshed Sources.
     """
-    sources = await source_crud.get_all() or []
-    return await refresh_sources(sources=sources)
+    sources = await source_crud.get_all()
+    return refresh_sources(sources=sources)
 
 
 @timeit
 async def refresh_sources(sources: list[Source]) -> list[Source]:
     """
-    Fetches new data for a list of sources from yt-dlp.
+    Fetches new data from yt-dlp for each Source.
 
     Args:
         sources: The list of sources to refresh.
 
     Returns:
-        The refreshed list of sources.
+        The list of refreshed Sources.
     """
     return [await source_crud.fetch_source(source_id=source.id) for source in sources]
+
+
+async def add_new_source_videos_from_fetched_videos(
+    fetched_videos: list[Video], db_source: Source
+) -> list[Video]:
+    """
+    Add new videos from a list of fetched videos to a source in the database.
+
+    Args:
+        fetched_videos: A list of Video objects fetched from a source.
+        db_source: The Source object in the database to add the new videos to.
+
+    Returns:
+        A list of Video objects that were added to the database.
+    """
+    db_video_ids = [video.id for video in db_source.videos]
+
+    # Add videos that were fetched, but not in the database.
+    added_videos = []
+    for fetched_video in fetched_videos:
+        if fetched_video.id not in db_video_ids:
+            added_videos.append(fetched_video)
+            await video_crud.create(in_obj=fetched_video)
+
+    return added_videos
+
+
+async def delete_orphaned_source_videos(
+    fetched_videos: list[Video], db_source: Source
+) -> list[Video]:
+    """
+    Delete videos that are no longer present in `fetched_videos`.
+
+    Args:
+        fetched_videos: The list of Videos to compare the videos against.
+        db_source: The source object in the database to delete videos from.
+
+    Returns:
+        The list of deleted Videos.
+    """
+    fetched_video_ids = [video.id for video in fetched_videos]
+
+    # Iterate through the videos in the source in the database
+    deleted_videos = []
+    for db_video in db_source.videos:
+        if db_video.id not in fetched_video_ids:
+            deleted_videos.append(db_video)
+            await video_crud.delete(id=db_video.id)
+
+    return deleted_videos
