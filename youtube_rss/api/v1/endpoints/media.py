3198a9d5c28cd4ef9a09201fc4c83133296cd72a
martokk <25113632+martokk@users.noreply.github.com>
2023-01-10 16:22:42 -0600

    Better error handling of media & reverse proxy

--- youtube_rss/api/v1/endpoints/media.py
+++ youtube_rss/api/v1/endpoints/media.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, HTTPException, Request, status
 from fastapi.responses import RedirectResponse, Response
+from loguru import logger
 
 from youtube_rss.core.proxy import reverse_proxy
+from youtube_rss.crud.exceptions import RecordNotFoundError
 from youtube_rss.crud.video import video_crud
 from youtube_rss.handlers import get_handler_from_string
 
@@ -10,12 +12,22 @@ router = APIRouter()
 
 @router.get("/{video_id}")
 @router.head("/{video_id}")
-async def reverse_proxy_video_id(video_id: str, request: Request) -> Response:
+async def handle_media(video_id: str, request: Request) -> Response:
     """
-    Streams the response of yt-dlp
+    Handles the repose for a media request by video_id.
+    Uses a reverse proxy if required by the handler.
     """
-    video = await video_crud.get(id=video_id)
+    try:
+        video = await video_crud.get(id=video_id)
+    except RecordNotFoundError as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The video_id '{video_id}' was not found.",
+        ) from e
+
     if video.media_url is None:
+        logger.error("The server has not yet retrieved a media_url from yt-dlp.")
         raise HTTPException(
             status_code=status.HTTP_202_ACCEPTED,
             detail="The server has not yet retrieved a media_url from yt-dlp.",
