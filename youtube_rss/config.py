900dfcf7c8c32a7cb8c059d899f4c097814c0e98
martokk <25113632+martokk@users.noreply.github.com>
2023-01-12 23:24:39 -0600

    cleanup

--- youtube_rss/config.py
+++ youtube_rss/config.py
@@ -16,7 +16,7 @@ SERVER_PORT = int(os.environ.get("SERVER_PORT", 5000))
 BASE_DOMAIN = str(os.environ.get("BASE_DOMAIN", f"{SERVER_IP}:{SERVER_PORT}"))
 BASE_URL = str(os.environ.get("BASE_URL", f"http://{BASE_DOMAIN}"))
 UVICORN_RELOAD = str(os.environ.get("UVICORN_RELOAD", "False")) == "True"
-UVICORN_ENTRYPOINT = str(os.environ["UVICORN_ENTRYPOINT"])
+UVICORN_ENTRYPOINT = str(os.environ.get("UVICORN_ENTRYPOINT"))
 
 # Proxy
 PROXY_HOST = str(os.environ.get("PROXY_HOST", "localhost"))
@@ -31,8 +31,8 @@ LOG_LEVEL = str(os.environ.get("LOG_LEVEL", "INFO")).upper()
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 ALGORITHM = "HS256"
-JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]  # should be kept secret
-JWT_REFRESH_SECRET_KEY = os.environ["JWT_REFRESH_SECRET_KEY"]  #
+JWT_SECRET_KEY = str(os.environ.get("JWT_SECRET_KEY"))
+JWT_REFRESH_SECRET_KEY = str(os.environ.get("JWT_REFRESH_SECRET_KEY"))
 
 # Notify
 TELEGRAM_API_TOKEN = str(os.environ.get("TELEGRAM_API_TOKEN"))
